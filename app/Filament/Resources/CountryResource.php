81ec7db615e0136e59abc27a3b4410e864ef6453
habijm <habijiyan111@gmail.com>
2024-07-18 20:16:38 +0700

    first commit

---
+++ app/Filament/Resources/CountryResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CountryResource\Pages;
+use App\Filament\Resources\CountryResource\RelationManagers;
+use App\Models\Country;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CountryResource extends Resource
+{
+    protected static ?string $model = Country::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-flag'; // https://heroicons.com/
+    
+    protected static ?string $navigationLabel = 'Country'; // default: Resource::$label
+
+    protected static ?string $modelLabel = 'Employees Country';     // default: Str::title(Str::snake(class_basename(static::getModel()), ' '))
+
+    public static ?string $navigationGroup = 'System Management'; // GROUP NAVIGATION
+
+    public static ?int $navigationSort = 1; // Urutan
+
+    // public static ?string $slug = 'employees-country'; // Nama URL
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCountries::route('/'),
+            'create' => Pages\CreateCountry::route('/create'),
+            'edit' => Pages\EditCountry::route('/{record}/edit'),
+        ];
+    }
+}
